c03116da7587ab0661496adb27c2dcfeac27efa3
GuidoHogenbirk <guido.hogenbirk@student.uva.nl>
2024-11-15 16:13:30 +0100

    smallangle finished

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -10,21 +10,22 @@ def cmd_group():
 
 
 @cmd_group.command()
-@click.option("-n", "--number", default=1)
+@click.option("-n", "--number", default=1, help="Number of steps between 0 and pi")
 def sin(number):
+    """calculates sin between 0 and pi in number of given steps"""
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 
 @cmd_group.command()
-@click.option("-n", "--number", default=1)
+@click.option("-n", "--number", default=1, help="Number of steps between 0 and pi")
 def tan(number):
+    """calculates tan between 0 and pi in number of given steps"""
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
 
 if __name__ == "__main__":
-    sin()
-    tan()
+    cmd_group()
